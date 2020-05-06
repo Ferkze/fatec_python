70f79b47bab5c504cb7441eb89678b4c9f3e4eef
Ferkze <ferkemartins@gmail.com>
2020-05-06 20:24:38 -0300

    adicionado exercicio1: funcao de maximo

---
+++ aulas/aula6/exercicio1.py
@@ -0,0 +1,10 @@
+def maximo(*valores):
+  "Retorna maior valor numérico passado nos argumentos"
+  max = valores[0]
+  for i in range(len(valores)):
+    val = valores[i]
+    if max <= val:
+      max = val
+  return max
+
+print(maximo(1,2,3,4,5,6,7,2,3,5,3,10,2,4,1,0))
