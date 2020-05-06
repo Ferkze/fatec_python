6599b1df8d4eba34da9d0e78edeebcaa894c4d0b
Ferkze <ferkemartins@gmail.com>
2020-05-06 20:49:19 -0300

    adicionado exercicio2: funcao maior_primo

---
+++ aulas/aula6/maior_primo.py
@@ -0,0 +1,20 @@
+def maior_primo(max=0):
+  "Retorna o maior número primo passado até o valor do argumento"
+
+  def primo(input):
+    "Verifica se o argumento passado é um número primo"
+    antecessores = list(range(2, input+1))
+    for i in antecessores:
+      if i == input:
+        return True
+      if input%i == 0:
+        return False
+
+  if max <= 2:
+    return
+  for i in range(3, max+1):
+    val = max - i + 3
+    if primo(val):
+      return val
+
+print(maior_primo(100))
