c7ffd765f8caf6e95481a23f6a618b9f0df0cb8a
Ferkze <ferkemartins@gmail.com>
2020-05-27 20:56:45 -0300

    adicionado exercicio de classe de quadrados

---
+++ aulas/aula10/quadrado/quadrado.py
@@ -0,0 +1,40 @@
+import math
+
+class Quadrado():
+	'Define um quadrado'
+
+	def __init__(self, lado):
+		self.setLado(lado)
+
+	def setLado(self, lado):
+		self.lado = lado
+
+	def getLado(self):
+		return self.lado
+
+	def calculaArea(self):
+		return self.lado * self.lado
+
+	def calculaPerimetro(self):
+		return self.lado *2
+
+	def cabeNaCaixa(self, m = 100):
+		quantidade = m / self.lado
+		if quantidade < 1:
+			print('O quadrado não cabe na caixa')
+			return
+
+		resto = m % self.lado
+		if resto == 0:
+			print('{} x quadrado cabe perfeitamente na caixa'.format(quantidade))
+		else:
+			print('{} x quadrado cabe na caixa, porém sobra {} de espaço'.format(math.floor(quantidade), resto))
+
+quadrado1 = Quadrado(5)
+quadrado1.cabeNaCaixa(100)
+
+quadrado2 = Quadrado(7)
+quadrado2.cabeNaCaixa(100)
+
+quadrado2 = Quadrado(120)	
+quadrado2.cabeNaCaixa(100)
