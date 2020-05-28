27cd7a481b3ca7a9ce69692e72fcff2dfb3784cd
Ferkze <ferkemartins@gmail.com>
2020-05-27 21:11:59 -0300

    adicionado método de transferencia na classe conta_corrente

--- aulas/aula10/conta_corrente/conta_corrente.py
+++ aulas/aula10/conta_corrente/conta_corrente.py
@@ -19,3 +19,30 @@ class ContaCorrente:
 			return 0
 
 		return self.saldo * tx_juros
+
+	def enviaTransferencia(self, conta, valor):
+		if self.saldo < valor:
+			return 'Saldo insuficiente'
+		
+		if conta.recebeTransferencia(valor):
+			self.saldo -= valor
+			return 'Transferencia bem sucedida'
+
+		return 'Transferencia mal sucedida'
+
+	def recebeTransferencia(self, valor):
+		if valor <= 0:
+			return False
+
+		self.saldo += valor
+		return True
+
+conta = ContaCorrente(100, 'Tales')
+conta.deposito(100)
+print(conta.saldo)
+conta.saque(200)
+print(conta.saldo)
+
+conta2 = ContaCorrente(200, 'Fabio', 200)
+resultado = conta2.enviaTransferencia(conta, 150)
+print(resultado)
