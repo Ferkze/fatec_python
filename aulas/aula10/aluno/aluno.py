fcac2671d229b76a37668f061921c6522463e2cd
Ferkze <ferkemartins@gmail.com>
2020-05-27 21:44:05 -0300

    adicionado testes de variáveis da classe Aluno

--- aulas/aula10/aluno/aluno.py
+++ aulas/aula10/aluno/aluno.py
@@ -1,7 +1,7 @@
 from pprint import pprint
 
 class Aluno:
-	def __init__(self, nome, idade, peso, altura, salario):
+	def __init__(self, nome, idade, peso, altura, salario = 0):
 		self.nome = nome
 		self.idade = idade
 		self.peso = peso
@@ -30,7 +30,36 @@ class Aluno:
 			else:
 				self.crescer((25 - anosAntes) * 0.5)
 
+	def setSalario(self, salario):
+		self.salario = salario
+
+	def demissao(self):
+		self.salario = 0
+
 	def aumentarSalarioPercentual(self, percentual):
 		if percentual <= 0:
-			return self.salario
-		return self.salario * (1+percentual)
+			return
+		self.salario *= (1+percentual)
+
+
+douglas = Aluno('Douglas Duarte',43, 67.0, 167)
+pprint(vars(douglas))
+
+douglas.engordar(1)
+pprint(vars(douglas))
+
+douglas.emagrecer(2)
+pprint(vars(douglas))
+
+douglas.crescer(3)
+pprint(vars(douglas))
+
+douglas.envelhecer(4)
+pprint(vars(douglas))
+
+fabio = Aluno('Fabio Martins', 19, 72.0, 172, 11200)
+fabio.aumentarSalarioPercentual(0.2)
+pprint(vars(fabio))
+fabio.demissao()
+fabio.setSalario(15000)
+pprint(vars(fabio))
