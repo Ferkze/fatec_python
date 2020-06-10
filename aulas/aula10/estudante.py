49679ac70f12d5a58e338873cdf75df52e697fba
Ferkze <ferkemartins@gmail.com>
2020-06-10 19:33:17 -0300

    remoção dos diretórios de classes da aula 10

--- aulas/aula10/estudante.py
+++
@@ -1,9 +0,0 @@
-class Estudante:
-	def __init__(self, nome):
-		self.nome = nome
-
-	def estudar(self, materia):
-		print("{} está estudando {}".format(self.nome, materia))
-
-fabio = Estudante("Fabio")
-fabio.estudar("Multimidia e Hipermidia")
