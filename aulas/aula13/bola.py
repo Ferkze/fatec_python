5630d080fb8fffa80c125bf2f7f434cc7beacd27
Ferkze <ferkemartins@gmail.com>
2020-06-17 20:23:31 -0300

    adicionado set de cores e validação de cores

--- aulas/aula13/bola.py
+++ aulas/aula13/bola.py
@@ -1,11 +1,21 @@
+cores = set(('preto', 'branco', 'vermelho', 'verde', 'azul'))
+
+
 class Bola:
     'Classe para definir uma bola'
 
-    def __init__(self, raio):
+    def __init__(self, raio, cor='sem cor'):
         self.raio = raio
-        self.cor = "sem cor"
+        if cor not in cores:
+            print('Cor {} não permitida, as cores permitidas são: {}'.format(cor, cores))
+            self.cor = ''
+        else:
+            self.cor = cor
 
     def trocaCor(self, cor):
+        if cor not in cores:
+            print('Cor {} não permitida, as cores permitidas são: {}'.format(cor, cores))
+            return
         try:
             if type(cor) == 'string':
                 raise TypeError
@@ -14,6 +24,9 @@ class Bola:
             print('Erro no tipo de valor atribuído à cor')
 
     def mostraCor(self):
+        if self.cor not in cores:
+            print('Cor indefinida ou não permitida, cores permitidas:', cores)
+            return
         try:
             if self.raio == 0:
                 raise ValueError
@@ -45,8 +58,11 @@ class Bola:
 bola = Bola(5)
 bola.trocaCor("Azul")
 bola.mostraCor()
+bola.trocaCor("azul")
+bola.mostraCor()
 bola.mostrarDiametro()
 bola.estourar()
+bola.mostrarDiametro()
 bola.mostrarRaio()
 bola.mostraCor()  # Exception
 bola.trocaCor(1)  # Exception
