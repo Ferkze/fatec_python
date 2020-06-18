06b636235b67b1d6914bc314c3f86f595d230260
Ferkze <ferkemartins@gmail.com>
2020-06-17 21:23:05 -0300

    adicionado exemplos de sets nas formas

--- aulas/aula13/quadrado.py
+++ aulas/aula13/quadrado.py
@@ -1,44 +1,66 @@
 import math
 
+cores = set(('preto', 'branco', 'vermelho', 'verde', 'azul'))
+tamanhos = set((1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144))
+
+
 class Quadrado():
-	'Define um quadrado'
+    'Define um quadrado'
 
-	def __init__(self, lado, cor):
-		self.trocarCor(cor)
-		self.setLado(lado)
+    def __init__(self, lado, cor):
+        self.trocarCor(cor)
+        self.setLado(lado)
 
-	def trocarCor(self, cor):
-		self.cor = cor
+    def trocarCor(self, cor):
+        if cor not in cores:
+            print('Cor {} não permitida, as cores permitidas são: {}'.format(cor, cores))
+            self.cor = ''
+        else:
+            self.cor = cor
 
-	def setLado(self, lado):
-		self.lado = lado
+    def setLado(self, lado):
+        if lado not in tamanhos:
+            print(
+                'Lado {} não permitido, os tamanhos permitidos são: {}'.format(lado, tamanhos))
+            self.lado = 0
+        else:
+            self.lado = lado
 
-	def getLado(self):
-		return self.lado
+    def getLado(self):
+        return self.lado
 
-	def calculaArea(self):
-		return self.lado * self.lado
+    def calculaArea(self):
+        return self.lado * self.lado
 
-	def calculaPerimetro(self):
-		return self.lado *2
+    def calculaPerimetro(self):
+        return self.lado * 2
 
-	def cabeNaCaixa(self, m = 100):
-		quantidade = m / self.lado
-		if quantidade < 1:
-			print('O quadrado não cabe na caixa')
-			return
+    def cabeNaCaixa(self, m=100):
+        try:
+            quantidade = m / self.lado
+            if quantidade < 1:
+                print('O quadrado não cabe na caixa')
+                return
 
-		resto = m % self.lado
-		if resto == 0:
-			print('{} x quadrado {} cabe perfeitamente na caixa'.format(quantidade, self.cor))
-		else:
-			print('{} x quadrado {} cabe na caixa, porém sobra {} de espaço'.format(math.floor(quantidade), self.cor, resto))
+            resto = m % self.lado
+            if resto == 0:
+                print('{} x quadrado {} cabe perfeitamente na caixa'.format(
+                    quantidade, self.cor))
+            else:
+                print('{} x quadrado {} cabe na caixa, porém sobra {} de espaço'.format(
+                    math.floor(quantidade), self.cor, resto))
+        except ZeroDivisionError:
+            print('Lado vazio...')
 
-quadrado1 = Quadrado(5, 'Azul')
+
+quadrado1 = Quadrado(5, 'azul')
 quadrado1.cabeNaCaixa(100)
 
-quadrado2 = Quadrado(7, 'Vermelho')
+quadrado2 = Quadrado(7, 'laranja')
 quadrado2.cabeNaCaixa(100)
 
-quadrado2 = Quadrado(120, 'Preto')	
-quadrado2.cabeNaCaixa(100)
+quadrado3 = Quadrado(55, 'preto')
+quadrado3.cabeNaCaixa(100)
+
+quadrado4 = Quadrado(144, 'branco')
+quadrado4.cabeNaCaixa(100)
--- aulas/aula13/retangulo.py
+++ aulas/aula13/retangulo.py
@@ -1,58 +1,80 @@
 import math
 
+cores = set(('preto', 'branco', 'vermelho', 'verde', 'azul'))
+tamanhos = set((1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144))
+
+
 class Retangulo():
-	'Define um Retangulo'
+    'Define um Retangulo'
 
-	def __init__(self, lado1, lado2):
-		self.setLados(lado1, lado2)
-		self.cor = ''
+    def __init__(self, lado1, lado2):
+        self.setLados(lado1, lado2)
+        self.cor = ''
 
-	def setLados(self, lado1, lado2):
-		self.lado1 = lado1
-		self.lado2 = lado2
+    def setLados(self, lado1, lado2):
+        if lado1 not in tamanhos:
+            print(
+                'Lado 1 {} não permitido, os tamanhos permitidos são: {}'.format(lado1, tamanhos))
+            self.lado1 = 0
+        else:
+            self.lado1 = lado1
+        if lado2 not in tamanhos:
+            print(
+                'Lado 2 {} não permitido, os tamanhos permitidos são: {}'.format(lado2, tamanhos))
+            self.lado2 = 0
+        else:
+            self.lado2 = lado2
 
-	def getLado1(self):
-		return self.lado1
+    def getLado1(self):
+        return self.lado1
 
-	def getLado2(self):
-		return self.lado2
+    def getLado2(self):
+        return self.lado2
 
-	def calculaArea(self):
-		return self.lado1 * self.lado2
+    def calculaArea(self):
+        return self.lado1 * self.lado2
 
-	def calculaPerimetro(self):
-		return 2 * (self.lado1 + self.lado2)
+    def calculaPerimetro(self):
+        return 2 * (self.lado1 + self.lado2)
 
-	def trocaCor(self, cor):
-		self.cor = cor
+    def trocaCor(self, cor):
+        if cor not in cores:
+            print('Cor {} não permitida, as cores permitidas são: {}'.format(cor, cores))
+            self.cor = ''
+        else:
+            self.cor = cor
 
-	def mostraCor(self):
-		if self.cor == '':
-			print('Retangulo sem cor')
-		else:
-			print('Retangulo {}'.format(self.cor))
+    def mostraCor(self):
+        if self.cor == '':
+            print('Retangulo sem cor')
+        else:
+            print('Retangulo {}'.format(self.cor))
 
-	def cabeNaCaixa(self, area = 100):
-		quantidade = area / self.calculaArea()
-		if quantidade < 1:
-			print('O Retangulo não cabe na caixa')
-			return
+    def cabeNaCaixa(self, area=100):
+        quantidade = area / self.calculaArea()
+        if quantidade < 1:
+            print('O Retangulo não cabe na caixa')
+            return
 
-		resto = area % self.calculaArea()
-		if resto == 0:
-			print('{} x Retangulo cabe perfeitamente na caixa'.format(quantidade))
-		else:
-			print('{} x Retangulo cabe na caixa, porém sobra {} de espaço'.format(math.floor(quantidade), resto))
+        resto = area % self.calculaArea()
+        if resto == 0:
+            print('{} x Retangulo cabe perfeitamente na caixa'.format(quantidade))
+        else:
+            print('{} x Retangulo cabe na caixa, porém sobra {} de espaço'.format(
+                math.floor(quantidade), resto))
+
 
 retangulo1 = Retangulo(5, 2)
 retangulo1.trocaCor('Preto')
 retangulo1.mostraCor()
 retangulo1.cabeNaCaixa(100)
 
-retangulo2 = Retangulo(7, 5)
+retangulo2 = Retangulo(8, 5)
+retangulo2.trocaCor('azul')
 retangulo2.mostraCor()
 retangulo2.cabeNaCaixa(100)
 
-retangulo3 = Retangulo(120, 2)	
+retangulo3 = Retangulo(144, 4)
+retangulo3.trocaCor('amarelo')
 retangulo3.mostraCor()
 retangulo3.cabeNaCaixa(100)
