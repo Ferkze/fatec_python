c109e9cd9a9a815322c7461e4ecd2b787d9c80e1
Ferkze <ferkemartins@gmail.com>
2020-06-17 21:01:05 -0300

    adicionado exemplos de sets nas classes de contas

--- aulas/aula13/conta_corrente.py
+++ aulas/aula13/conta_corrente.py
@@ -1,47 +1,66 @@
+contas = set()
+
+
 class ContaCorrente:
 
-	def __init__(self, numero, nomeCorrentista, saldo=0.0):
-		self.numero = numero
-		self.alterarNome(nomeCorrentista)
-		self.saldo = saldo
+    def __init__(self, numero, nomeCorrentista, saldo=0.0):
+        if numero in contas:
+            raise Exception
+        self.numero = numero
+        self.alterarNome(nomeCorrentista)
+        self.saldo = saldo
 
-	def getNumero(self):
-		return self.numero
+        contas.add(numero)
 
-	def getSaldo(self):
-		return self.saldo
+    def getNumero(self):
+        return self.numero
 
-	def alterarNome(self, nomeCorrentista):
-		self.nomeCorrentista = nomeCorrentista
+    def getSaldo(self):
+        return self.saldo
 
-	def deposito(self, valor):
-		self.saldo += valor
+    def alterarNome(self, nomeCorrentista):
+        self.nomeCorrentista = nomeCorrentista
 
-	def saque(self, valor):
-		self.saldo -= valor
+    def deposito(self, valor):
+        self.saldo += valor
 
-	def calculaJurosDeSaldoNegativo(self, tx_juros):
-		if self.saldo >= 0:
-			return 0
+    def saque(self, valor):
+        self.saldo -= valor
 
-		return self.saldo * tx_juros
+    def saqueATM(self, valor):
+        opcoes_de_saque = set((5, 10, 20, 50, 100))
+        if valor not in opcoes_de_saque:
+            print('Saque de valor {} não permitido, opcoes: {}'.format(
+                valor, opcoes_de_saque))
+            return
+        self.saldo -= valor
 
-	def enviaTransferencia(self, conta, valor):
-		if self.saldo < valor:
-			return 'Saldo insuficiente'
-		
-		if conta.recebeTransferencia(valor):
-			self.saldo -= valor
-			return 'Transferencia bem sucedida para a conta {}'.format(conta.getNumero())
+    def calculaJurosDeSaldoNegativo(self, tx_juros):
+        if self.saldo >= 0:
+            return 0
 
-		return 'Transferencia mal sucedida para a conta {}'.format(conta.getNumero())
+        return self.saldo * tx_juros
 
-	def recebeTransferencia(self, valor):
-		if valor <= 0:
-			return False
+    def enviaTransferencia(self, conta, valor):
+        if self.saldo < valor:
+            return 'Saldo insuficiente'
 
-		self.saldo += valor
-		return True
+        if conta.getNumero() not in contas:
+            return 'Transferencia mal sucedida para a conta {} inválida'.format(conta.getNumero())
+
+        if conta.recebeTransferencia(valor):
+            self.saldo -= valor
+            return 'Transferencia bem sucedida para a conta {}'.format(conta.getNumero())
+
+        return 'Transferencia mal sucedida para a conta {}'.format(conta.getNumero())
+
+    def recebeTransferencia(self, valor):
+        if valor <= 0:
+            return False
+
+        self.saldo += valor
+        return True
+
 
 conta = ContaCorrente(100, 'Tales')
 conta.deposito(100)
@@ -56,3 +75,6 @@ resultado = conta2.enviaTransferencia(conta, 150)
 print(resultado)
 
 print('Saldo da conta {}: {}'.format(conta.getNumero(), conta.getSaldo()))
+
+conta3 = ContaCorrente(100, 'Teste', 200)
+print(contas)
--- aulas/aula13/conta_investimento.py
+++ aulas/aula13/conta_investimento.py
@@ -5,9 +5,18 @@ class ContaInvestimento:
         self.alterarNome(nomeCorrentista)
         self.taxaJuros = taxaJuros
         self.saldo = saldo
+        self.modalidades = set((
+            'credito',
+            'debito',
+            'poupança',
+            'investimentos'
+        ))
 
     def alterarNome(self, nomeCorrentista):
         self.nomeCorrentista = nomeCorrentista
+
+    def addModalidade(self, modalidade):
+        self.modalidades.add(modalidade)
 
     def deposito(self, valor):
         try:
@@ -18,15 +27,21 @@ class ContaInvestimento:
             print('Erro no valor provisionado de deposito: {}'.format(err))
 
     def saque(self, valor):
+        opcoes_de_saque = set((5, 10, 20, 50, 100))
         try:
             if valor <= 0:
                 raise ValueError
+            if valor not in opcoes_de_saque:
+                print('Saque de valor {} não permitido, opcoes: {}'.format(
+                    valor, opcoes_de_saque))
+                return
             self.saldo -= valor
         except ValueError as err:
             print('Erro no valor provisionado de saque: {}'.format(err))
 
     def adicionaJuros(self):
-        self.saldo += (self.saldo * (self.taxaJuros / 100.0))
+        if self.modalidades.issuperset(set(('debito', 'poupança'))):
+            self.saldo += (self.saldo * (self.taxaJuros / 100.0))
 
     def ajustarTaxaJuros(self, taxa):
         'Ajusta a taxa de juros'
@@ -51,7 +66,8 @@ class ContaInvestimento:
 
     def adicionaPremiacao(self):
         'Premia a conta dobrando seu saldo'
-        self.saldo *= 2
+        if self.modalidades.issuperset(set(('debito', 'premiação'))):
+            self.saldo *= 2
 
 
 # TESTE DA CLASSE
@@ -68,6 +84,7 @@ conta.adicionaJuros()
 print(conta.saldo)
 conta.adicionaJuros()
 print(conta.saldo)
+conta.addModalidade('premiação')
 conta.adicionaPremiacao()
 print(conta.saldo)
 
