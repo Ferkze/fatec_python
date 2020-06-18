ff740642c2fd8e7030c63860c69c66c7da84a7e6
Ferkze <ferkemartins@gmail.com>
2020-06-17 21:34:18 -0300

    adicionado exemplos de sets de tipos de tv e marcas

--- aulas/aula13/tv.py
+++ aulas/aula13/tv.py
@@ -1,55 +1,72 @@
+marcas = set(
+    ['Philips', 'Semp Toshiba', 'TCL', 'Samsung']
+)
+tipos = set(
+    ['LED', 'Plasma', 'OLED', 'LCD']
+)
+
+
 class TV:
-	'Classe TV'
-	def __init__(self, marca, tipo, volume = 0, brilho = 10, canal = 0):
-		self.setMarca(marca)
-		self.setTipo(tipo)
-		self.volume = volume
-		self.brilho = brilho
-		self.setCanal(canal)
+    'Classe TV'
 
-	def setMarca(self, marca):
-		'Configura a marca da TV'
-		if marca == '':
-			self.marca = 'Não especificado'
-		elif (marca not in ['Philips', 'Semp Toshiba', 'TCL', 'Samsung']):
-			self.marca = 'Marca desconhecida'
-		else:
-			self.marca = marca
+    def __init__(self, marca, tipo, volume=0, brilho=10, canal=0, lancamento=False):
+        if lancamento:
+            self.comemoraLancamento(marca, tipo)
+        self.setMarca(marca)
+        self.setTipo(tipo)
+        self.volume = volume
+        self.brilho = brilho
+        self.setCanal(canal)
 
-	def setTipo(self, tipo):
-		'Configura o tipo da TV'
-		if tipo == '':
-			self.tipo = 'Não especificado'
-		elif (tipo not in ['LED', 'Plasma', 'OLED', 'LCD']):
-			self.tipo = 'Tipo desconhecido'
-		else:
-			self.tipo = tipo
+    def comemoraLancamento(self, marca, tipo):
+        print('Lancamento da TV %s %s' % (marca, tipo))
+        marcas.add(marca)
+        tipos.add(tipo)
 
-	def setCanal(self, canal):
-		'Seleciona o canal passado de 0 a 100'
-		if (canal < 0) or (canal > 100):
-			return
-		self.canal = canal
+    def setMarca(self, marca):
+        'Configura a marca da TV'
+        if marca == '':
+            self.marca = 'Não especificado'
+        elif (marca not in marcas):
+            self.marca = 'Marca desconhecida'
+        else:
+            self.marca = marca
 
-	def aumentaVolume(self):
-		'Aumenta o volume de 1 em 1 até o máximo de 100'
-		if (self.volume < 100):
-			self.volume += 1
+    def setTipo(self, tipo):
+        'Configura o tipo da TV'
+        if tipo == '':
+            self.tipo = 'Não especificado'
+        elif (tipo not in tipos):
+            self.tipo = 'Tipo desconhecido'
+        else:
+            self.tipo = tipo
 
-	def diminuiVolume(self):
-		'Aumenta o volume de 1 em 1 até o mínimo de 1'
-		if (self.volume > 0):
-			self.volume -= 1
+    def setCanal(self, canal):
+        'Seleciona o canal passado de 0 a 100'
+        if (canal < 0) or (canal > 100):
+            return
+        self.canal = canal
 
-	def aumentaBrilho(self):
-		'Aumenta o brilho de 1 em 1 até o máximo de 10'
-		if (self.brilho < 10):
-			self.brilho += 1
+    def aumentaVolume(self):
+        'Aumenta o volume de 1 em 1 até o máximo de 100'
+        if (self.volume < 100):
+            self.volume += 1
 
-	def diminuiBrilho(self):
-		'Diminui o brilho de 1 em 1 até o mínimo de 1'
-		if (self.brilho > 1):
-			self.brilho -= 1
+    def diminuiVolume(self):
+        'Aumenta o volume de 1 em 1 até o mínimo de 1'
+        if (self.volume > 0):
+            self.volume -= 1
+
+    def aumentaBrilho(self):
+        'Aumenta o brilho de 1 em 1 até o máximo de 10'
+        if (self.brilho < 10):
+            self.brilho += 1
+
+    def diminuiBrilho(self):
+        'Diminui o brilho de 1 em 1 até o mínimo de 1'
+        if (self.brilho > 1):
+            self.brilho -= 1
+
 
 # TESTE DA CLASSE
 tv = TV('Samsung', 'LCD', volume=63, brilho=8, canal=13)
@@ -65,4 +82,10 @@ tv.diminuiBrilho()
 tv.diminuiBrilho()
 tv.aumentaBrilho()
 
-print("[TV %s %s] Canal: %d - Volume: %d - Brilho: %d" % (tv.tipo, tv.marca, tv.canal, tv.volume, tv.brilho))
+print("[TV %s %s] Canal: %d - Volume: %d - Brilho: %d" %
+      (tv.tipo, tv.marca, tv.canal, tv.volume, tv.brilho))
+
+tv2 = TV('LG', 'LED IPS', volume=63, brilho=8, canal=13, lancamento=True)
+
+print("[TV %s %s] Canal: %d - Volume: %d - Brilho: %d" %
+      (tv2.tipo, tv2.marca, tv2.canal, tv2.volume, tv2.brilho))
